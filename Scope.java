b3591cceffd57a1e272f8971776ead6af724a4a7
aszellem <aszellemek@gmail.com>
2022-08-23 17:33:01 +0200

    done

--- Scope.java
+++ Scope.java
@@ -1,5 +1,16 @@
 public class Scope {
+    static int dogs = 5;
+
     public static void main(String[] args) {
-        
+        System.out.println(dogs);
+        someFunction();
+    }
+
+    /*
+     * void function is not gonna return anyting
+     */
+
+    public static void someFunction(){
+        System.out.println(dogs);
     }
 }
