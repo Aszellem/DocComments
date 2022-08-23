a13deb61eef13c74ccbd48912883511a7ca0a5fb
aszellem <aszellemek@gmail.com>
2022-08-23 17:48:47 +0200

    Math functions

---
+++ BuildInFunctions.java
@@ -0,0 +1,18 @@
+public class BuildInFunctions {
+    public static void main(String[] args) {
+        /**
+         * A good developer does not memorize code, 
+         * a good developer uses the internet. 
+         * Read documentation and find resources.
+         */
+
+        double sine = Math.sin(1.2);
+        System.out.println("The sin of 1.2 is : " + sine);
+
+        double power = Math.pow(2, 4); //1. base, 2. exponent
+        System.out.println("2 to the power of 2 is : " + power);
+
+        double random = Math.random() * 10 ;
+        System.out.println("The random value is: " + random);
+    }
+}
